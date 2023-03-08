fe39914441d07f2682f643a1351c91c89cafc3e7
PeterBarboza <pedro.barboza.dev@gmail.com>
2023-03-08 03:37:23 -0300

    feat: Input disable prop and style logic updated

--- web/components/atoms/ModalSubmitInput/index.tsx
+++ web/components/atoms/ModalSubmitInput/index.tsx
@@ -1,21 +1,32 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from "react"
 import styles from "./styles.module.scss"
 
-type props = {
+interface props extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   inputDisplayText: string
   isLoading?: boolean
 }
 
 export function ModalSubmitInput({
   inputDisplayText,
-  isLoading
+  isLoading,
+  ...inputProps
 }: props) {
   return (
     <div className={styles.modalSubmitInput}>
       <input
         type="submit"
-        className={`${styles.submitInput} ${isLoading ? styles.disabledInput : ""}`}
+        className={`${styles.submitInput} ${inputProps?.disabled || isLoading ? styles.disabledInput : ""}`}
         value={inputDisplayText}
-        disabled={isLoading}
+        disabled={inputProps?.disabled || isLoading}
+        style={
+          inputProps?.disabled ?
+            { cursor: "not-allowed" }
+            : isLoading ?
+              { cursor: "progress" }
+              :
+              {}
+        }
+        {...inputProps}
       />
     </div>
   )
