7e60b06e76e7c9d24412e50b6fb093f559e13683
PeterBarboza <pedro.barboza.dev@gmail.com>
2023-10-25 18:39:22 -0300

    fix: Toast font-size corrected

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />      
         <ToastContainer
           style={{
-            fontSize: "1.4rem"
+            fontSize: "1rem"
           }}
         />
       </VerifyAuth>
