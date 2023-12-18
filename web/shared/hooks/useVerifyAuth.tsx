88ea416a7947da3dc8fd8b30864b233a36e6ae3d
PeterBarboza <pedro.barboza.dev@gmail.com>
2023-12-17 23:47:12 -0300

    feat: recursion added to repeat the process if an unexpected error occurs

--- web/shared/hooks/useVerifyAuth.tsx
+++ web/shared/hooks/useVerifyAuth.tsx
@@ -12,46 +12,63 @@ type props = {
 type authResult = "pending" | "success" | "fail" | null
 
 export function useVerifyAuth({ errorCallback, successCallback }: props) {
-  const { setAuthContextData } = useContext(AuthContext)
+  const { accessToken, userData, setAuthContextData } = useContext(AuthContext)
   const [isLoading, setIsLoading] = useState(true)
   const [authResult, setAuthResult] = useState<authResult>(null)
 
-  const getCredentials = useCallback(async (refreshToken: string) => {
-    try {
-      setIsLoading(true)
-      setAuthResult("pending")
-      const authServices = new AuthServiceFactory().handle()
-  
-      const result = await authServices.authWithRefreshToken(refreshToken)
-  
-      if(result.status !== 200) {
-        throw new Error("RT Auth failed")
+  const getCredentials = useCallback(
+    async (refreshToken: string, executions = 1): Promise<void> => {
+      try {
+        setIsLoading(true)
+        setAuthResult("pending")
+        const authServices = new AuthServiceFactory().handle()
+    
+        const result: any = await authServices.authWithRefreshToken(refreshToken)
+    
+        if(result.status !== 200) {
+          if((result.status !== 401) && (result?.data?.requestNewToken !== false)) {
+            if(executions >= 3) {
+              return
+            }
+            return await getCredentials(refreshToken, executions++)
+          }
+
+          throw new Error("RT Auth failed")
+        }
+
+        localStorage.setItem(
+          SHARED_CONSTANTS.localStorage.refreshTokenLabel, 
+          result.data.refreshToken
+        )
+
+        setAuthContextData!({
+          accessToken: result.data?.accessToken,
+          userData: result.data?.user
+        })
+
+        setAuthResult("success")
+
+        if(successCallback) {
+          successCallback()
+        }
+      } catch (error) {
+        setAuthResult("fail")
+        if(errorCallback) {
+          errorCallback()
+        }
+      } finally {
+        setIsLoading(false)
       }
-
-      localStorage.setItem(
-        SHARED_CONSTANTS.localStorage.refreshTokenLabel, 
-        result.data.refreshToken
-      )
-
-      setAuthContextData!({
-        accessToken: result.data?.accessToken,
-        userData: result.data?.user
-      })
-
-      setAuthResult("success")
-
-      if(successCallback) {
-        successCallback()
-      }
-    } catch (error) {
-      setAuthResult("fail")
-      if(errorCallback) {
-        errorCallback()
-      }
-    } finally {
-      setIsLoading(false)
-    }
-  }, [])
+    }, 
+    [
+      accessToken,
+      userData, 
+      setAuthContextData, 
+      successCallback, 
+      errorCallback, 
+      setAuthResult
+    ]
+  )
 
   useEffect(() => {
     const rt = localStorage.getItem(SHARED_CONSTANTS.localStorage.refreshTokenLabel)
@@ -60,7 +77,7 @@ export function useVerifyAuth({ errorCallback, successCallback }: props) {
       getCredentials(rt)
       return
     }
-  }, [])
+  }, [accessToken])
 
   return {
     isLoading,
