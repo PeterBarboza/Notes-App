d03eae474bad0ac5b8c2bd31f01d6246b1f6240d
PeterBarboza <pedro.barboza.dev@gmail.com>
2023-12-17 23:16:48 -0300

    feat: delete note added; tool button hover style updated

--- web/components/organisms/FullNote/index.tsx
+++ web/components/organisms/FullNote/index.tsx
@@ -1,12 +1,16 @@
 import { format } from "date-fns"
 import { ptBR } from "date-fns/locale"
 import Link from "next/link"
-import { useContext, useMemo, useState } from "react"
+import { useRouter } from "next/router"
+import { useCallback, useContext, useEffect, useMemo, useState } from "react"
 import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai"
+import { toast } from "react-toastify"
+
 import { AuthContext } from "../../../contexts/authContext"
+import { NotesServiceFactory } from "../../../services/factories/notesServiceFactory"
+import { EditNoteModal } from "../../modals/EditNoteModal"
 
 import { Note } from "../../../interface/schemas"
-import { EditNoteModal } from "../../modals/EditNoteModal"
 
 import styles from "./styles.module.scss"
 
@@ -22,6 +26,7 @@ export function FullNote({
 }: Note) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { accessToken, userData } = useContext(AuthContext)
+  const router = useRouter()
   const lastUpdate = useMemo(() => {
     try {
       return format(new Date(updatedAt || createdAt), "dd/MM/yyyy - hh:mm", {
@@ -32,8 +37,69 @@ export function FullNote({
     }
   }, [updatedAt, createdAt])
 
+  const deleteNote = useCallback(async () => {
+    const toastId = toast.loading("Deletando nota...")
+
+    try {
+      const notesService = new NotesServiceFactory().handle()
+  
+      notesService.accessToken = accessToken
+
+      const result: any = await notesService.delete(id)
+
+       if(result?.data?.deletedCount === 1) {
+        toast.update(toastId, { 
+          render: "Nota deletada com sucesso", 
+          type: "success", 
+          isLoading: false,
+          autoClose: 1500,
+          closeOnClick: true,
+          closeButton: true,
+        });
+
+        setTimeout(() => router.push(`/app/usuarios/${author.username}`), 1500)
+        
+        return
+      }
+      
+      if(result.data?.message) {
+        toast.update(toastId, { 
+          render: result.data?.message, 
+          type: "error", 
+          isLoading: false,
+          autoClose: 5000,
+          closeOnClick: true,
+          closeButton: true,
+        })
+        return
+      }
+
+      toast.update(toastId, { 
+        render: "Erro ao deletar nota", 
+        type: "error", 
+        isLoading: false,
+        autoClose: 5000,
+        closeOnClick: true,
+        closeButton: true,
+      });
+    } catch (error) {
+       toast.update(toastId, { 
+        render: "Erro ao deletar nota", 
+        type: "error", 
+        isLoading: false,
+        autoClose: 5000,
+        closeOnClick: true,
+        closeButton: true,
+      });
+    }
+  }, [id])
+
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
+
+  useEffect(() => {
+    // console.log(window.history)
+  }, [])
 
   return (
     <main className={styles.fullNote}>
@@ -43,7 +109,7 @@ export function FullNote({
             <div className={styles.editNote} onClick={() => openModal()}>
               <AiOutlineEdit size={20} color={"#ffffff"}/>
             </div>
-            <div className={styles.deleteNote}>
+            <div className={styles.deleteNote} onClick={() => deleteNote()}>
               <AiOutlineDelete size={20} color={"#ffffff"}/>
             </div>
           </div>
