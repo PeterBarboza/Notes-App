5e6a4e21002d3baf018b0e0aa1f3af4c93e80401
PeterBarboza <pedro.barboza.dev@gmail.com>
2023-12-17 23:22:45 -0300

    feat: no notes found logic added

--- web/components/organisms/NotesBoard/index.tsx
+++ web/components/organisms/NotesBoard/index.tsx
@@ -1,27 +1,45 @@
+import { BiBlock } from "react-icons/bi"
+
 import { NoteCard } from "../../molecules/NoteCard"
 import { NotesBox } from "../../molecules/NotesBox"
 
 import { Note } from "../../../interface/schemas"
 
 import styles from "./styles.module.scss"
+import { useRouter } from "next/router"
 
 type props = {
-  notes: Note[]
+  notes: Note[] | null
 }
 
 export function NotesBoard({ notes }: props) {
+  const router = useRouter()
+
   return (
     <main className={styles.notesBoard}>
-      <NotesBox>
-        {notes.map(note => {
-          return (
-            <NoteCard
-              key={note.id}
-              {...note}
-            />
-          )
-        })}
-      </NotesBox>
+      {
+        notes === null?
+          <div className={styles.noNotesFound}>
+            <BiBlock size={80} color="#9b9b9b"/>
+            <div>
+              <p>Nenhuma nota foi encontrada em:</p>
+              <p>{router.asPath}</p>
+            </div>
+          </div>
+          :
+          <NotesBox>
+            {
+              notes.map(note => {
+                return (
+                  <NoteCard
+                    key={note.id}
+                    {...note}
+                  />
+                )
+              })
+            }
+          </NotesBox>
+      }
     </main>
   )
 }
